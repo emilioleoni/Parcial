14cd8a61ddd9ac0b3ba309f1117094cdcf2e4a2a
ezikaplan <96097788+ezikaplan@users.noreply.github.com>
2023-04-14 11:05:00 -0300

    mapa de nuevo

--- Scripts/chart_1.js
+++ Scripts/chart_1.js
@@ -1,42 +1,43 @@
 const mapaFetch = d3.json('CSVS/barrios-caba.geojson')
 const dataFetch = d3.dsv(';', 'CSVS/147_vehiculos_mal_estacionados.csv', d3.autoType)
-// Define tu token de acceso de Mapbox
-const mapboxAccessToken = 'pk.eyJ1IjoiZXppa2FwbGFuIiwiYSI6ImNsZ2VqMXN3bjA3Y2EzZnBzdmsxc2cyamQifQ.t-IRWfIEzRrI7UM9liJ4uQ';
 
 Promise.all([mapaFetch, dataFetch]).then(([barrios, data]) => {
-
-  // Agrupamos reclamos x barrio
-  const estacionadosPorBarrio = d3.group(data, d => d.domicilio_barrio);
-
-  // Crear el mapa coroplético con Plotly
-  const mapFig = {
-    data: [{
-      type: 'choroplethmapbox',
-      geojson: barrios,
-      locations: barrios.features.map(d => d.properties.BARRIO),
-      z: barrios.features.map(d => estacionadosPorBarrio.get(d.properties.BARRIO).length),
-      colorscale: 'YlOrBr',
-      colorbar: {
-        title: {
-          text: 'Cantidad de denuncias'
-        }
-      },
-      hovertemplate: '<b>%{location}</b><br>' +
-                     'Denuncias: %{z}<br>' +
-                     '<extra></extra>'
-    }],
-    layout: {
-      mapbox: {
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: { lon: -58.44, lat: -34.56 },
-        zoom: 10,
-        accesstoken: mapboxAccessToken // Agregar el token de acceso de Mapbox aquí
-      },
+  
+  /* Agrupamos reclamos x barrio */
+  const estacionadosPorBarrio = d3.group(data, d => d.domicilio_barrio) // crea un Map
+  console.log('estacionadosPorBarrio', estacionadosPorBarrio)
+  
+  /* Mapa Coroplético */
+  let chartMap = Plot.plot({
+    // https://github.com/observablehq/plot#projection-options
+    projection: {
+      type: 'mercator',
+      domain: barrios, // Objeto GeoJson a encuadrar
     },
-  };
+    color: {
+      // Quantize continuo (cant. denuncias) -> discreto (cant. colores)
+      type: 'quantize', 
+      n: 5,
+      scheme: 'ylorbr',
+      label: 'Cantidad de denuncias',
+      legend: true,
+    },
+    marks: [
+      Plot.geo(barrios, {
+        fill: d => {
+          let nombreBarrio = d.properties.BARRIO
+          let cantReclamos = estacionadosPorBarrio.get(nombreBarrio).length
+          d.properties.cant=cantReclamos.toString()
 
-  // Mostrar el mapa coroplético en el DOM
-  Plotly.newPlot('chart_1', mapFig);
+          return cantReclamos
+        },
+        stroke: '#ccc',
+        title: d => `${d.properties.BARRIO}\n${estacionadosPorBarrio.get(d.properties.BARRIO).length} denuncias`,
+
+      }),
+    ],
+  })
+
+  /* Agregamos al DOM la visualización chartMap */
+  d3.select('#chart_1').append(() => chartMap)
 })
-
-
