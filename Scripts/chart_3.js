45187845f42df6a6ea76eda5d11677762057ee60
ezikaplan <96097788+ezikaplan@users.noreply.github.com>
2023-04-19 17:37:52 -0300

    Cambio grosor lineas

--- Scripts/chart_3.js
+++ Scripts/chart_3.js
@@ -1,56 +1,49 @@
-// Load the first CSV data
-d3.csv('CSVS/multas_por_hstotal.csv').then(function(data1) {
-    // Create the chart
-    data1.forEach(function(d) {
+  d3.csv('CSVS/multas_por_hs.csv').then(function(data2) {
+    data2.forEach(function(d) {
       d.hora = +d.hora;
       d.cantidad = +d.cantidad;
     });
-d3.csv('CSVS/multas_por_hs.csv').then(function(data2) {
-  // Process the data
-  data2.forEach(function(d) {
-      d.hora = +d.hora;
-      d.cantidad = +d.cantidad;
-  });
-    let chart=Plot.plot({
+
+    const colorScale = d3.scaleSequential(interpolateYlOrBr)
+      .domain([0, d3.max(data1.concat(data2), d => d.cantidad)]);
+
+    let chart = Plot.plot({
       marginTop: 40,
       marginBottom: 40,
       grid: true,
-      width:1400,
+      width: 1400,
       x: {
         label: "Hora del dia",
         domain: [0, 23],
         tickValues: d3.range(0, 24),
         tickFormat: x => x + ":00",
-        
       },
       y: {
         label: "Cantidad de multas",
         domain: [0, 60],
       },
       marks: [
-        Plot.line(data1, {x: "hora", y: "cantidad", stroke: "orange"}),
-        Plot.dot(data1, {x: "hora", y: "cantidad", fill: "orange"}),
-        Plot.line(data2, {x: "hora", y: "cantidad", stroke: "maroon"}),
-        Plot.dot(data2, {x: "hora", y: "cantidad", fill: "maroon"}),
+        Plot.line(data1, {x: "hora", y: "cantidad", stroke: d => colorScale(d.cantidad), strokeWidth: 5}),
+        Plot.dot(data1, {x: "hora", y: "cantidad", fill: d => colorScale(d.cantidad)}),
+        Plot.line(data2, {x: "hora", y: "cantidad", stroke: d => colorScale(d.cantidad),strokeWidth: 5}),
+        Plot.dot(data2, {x: "hora", y: "cantidad", fill: d => colorScale(d.cantidad)}),
         Plot.text(["17:00"], {
           x: 17,
           y: data1.find(d => d.hora === 17).cantidad + 5,
           fontWeight: "bold",
           fontSize: 20,
-          fill: "orange",
+          fill: d => colorScale(d.cantidad),
         }),
         Plot.text(["12:00"], {
           x: 12,
           y: data2.find(d => d.hora === 12).cantidad + 5,
           fontWeight: "bold",
-          fontSize: 16,
-          fill: "maroon",
+          fontSize: 20,
+          fill: d => colorScale(d.cantidad),
         })
       ]
-      
     });
-      
 
     d3.select('#chart_3').append(() => chart);
   });
-});
+
