76ee7f75724ff2a4b8056ed9c091108f2ba6db2f
ezikaplan <96097788+ezikaplan@users.noreply.github.com>
2023-04-19 17:45:22 -0300

    aumento grosor g3 corregido

--- Scripts/chart_3.js
+++ Scripts/chart_3.js
@@ -1,11 +1,17 @@
+// Load the first CSV data
+d3.csv('CSVS/multas_por_hstotal.csv').then(function(data1) {
+  // Create the chart
+  data1.forEach(function(d) {
+    d.hora = +d.hora;
+    d.cantidad = +d.cantidad;
+  });
+
   d3.csv('CSVS/multas_por_hs.csv').then(function(data2) {
+    // Process the data
     data2.forEach(function(d) {
       d.hora = +d.hora;
       d.cantidad = +d.cantidad;
     });
-
-    const colorScale = d3.scaleSequential(interpolateYlOrBr)
-      .domain([0, d3.max(data1.concat(data2), d => d.cantidad)]);
 
     let chart = Plot.plot({
       marginTop: 40,
@@ -23,27 +29,27 @@
         domain: [0, 60],
       },
       marks: [
-        Plot.line(data1, {x: "hora", y: "cantidad", stroke: d => colorScale(d.cantidad), strokeWidth: 5}),
-        Plot.dot(data1, {x: "hora", y: "cantidad", fill: d => colorScale(d.cantidad)}),
-        Plot.line(data2, {x: "hora", y: "cantidad", stroke: d => colorScale(d.cantidad),strokeWidth: 5}),
-        Plot.dot(data2, {x: "hora", y: "cantidad", fill: d => colorScale(d.cantidad)}),
+        Plot.line(data1, {x: "hora", y: "cantidad", stroke: "orange", strokeWidth: 5}),
+        Plot.dot(data1, {x: "hora", y: "cantidad", fill: "orange"}),
+        Plot.line(data2, {x: "hora", y: "cantidad", stroke: "maroon",strokeWidth: 5}),
+        Plot.dot(data2, {x: "hora", y: "cantidad", fill: "maroon"}),
         Plot.text(["17:00"], {
           x: 17,
           y: data1.find(d => d.hora === 17).cantidad + 5,
           fontWeight: "bold",
           fontSize: 20,
-          fill: d => colorScale(d.cantidad),
+          fill: "orange",
         }),
         Plot.text(["12:00"], {
           x: 12,
           y: data2.find(d => d.hora === 12).cantidad + 5,
           fontWeight: "bold",
           fontSize: 20,
-          fill: d => colorScale(d.cantidad),
+          fill: "maroon",
         })
       ]
     });
 
     d3.select('#chart_3').append(() => chart);
   });
-
+});
