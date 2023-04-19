3e97896e977ae1e545e7803d650cac414ea5237f
emilioleoni <emilioleoni12@hotmail.com>
2023-04-19 12:24:51 -0300

    Tercer grafico

--- Scripts/chart_3.js
+++ Scripts/chart_3.js
@@ -1,62 +1,53 @@
 // Load the first CSV data
-Plotly.d3.csv('CSVS/multas_por_hstotal.csv', function(err, data1) {
-  if (err) throw err;
-
-  // Extract the hora and cantidad columns from the first CSV data
-  var horas1 = data1.map(function(row) {
-    return row.hora;
-  });
-
-  var cantidades1 = data1.map(function(row) {
-    return row.cantidad;
-  });
-
-  // Load the second CSV data
-  Plotly.d3.csv('CSVS/multas_por_hs.csv', function(err, data2) {
-    if (err) throw err;
-
-    // Extract the hora and cantidad columns from the second CSV data
-    var horas2 = data2.map(function(row) {
-      return row.hora;
+d3.csv('CSVS/multas_por_hstotal.csv').then(function(data1) {
+    // Create the chart
+    data1.forEach(function(d) {
+      d.hora = +d.hora;
+      d.cantidad = +d.cantidad;
     });
-
-    var cantidades2 = data2.map(function(row) {
-      return row.cantidad;
-    });
-
-    // Create two line graphs with markers using Plotly
-    var trace1 = {
-      x: horas1,
-      y: cantidades1,
-      mode: 'lines+markers', // Include markers along with lines
-      type: 'scatter',
-      name: 'Toda la semana',
-      line: {color: "orange"},
-      marker: {size: 6, color: 'orange'} // Set marker size and color
-    };
-
-    var trace2 = {
-      x: horas2, 
-      y: cantidades2,
-      mode: 'lines+markers', // Include markers along with lines
-      type: 'scatter',
-      name: 'Los jueves',
-      line: {color: 'maroon'},
-      marker: {size: 6, color: 'maroon'} // Set marker size and color
-    };
-
-    var layout = {
-      title: 'La bronca de las 17hs',
-      xaxis: {
-        title: 'Hora',
-        range: [0, 24],
-        tickmode: 'linear',
+d3.csv('CSVS/multas_por_hs.csv').then(function(data2) {
+  // Process the data
+  data2.forEach(function(d) {
+      d.hora = +d.hora;
+      d.cantidad = +d.cantidad;
+  });
+    let chart=Plot.plot({
+      grid: true,
+      width:1400,
+      x: {
+        label: "Hora del dia",
+        domain: [0, 23],
+        tickValues: d3.range(0, 24),
+        tickFormat: x => x + ":00"
       },
-      yaxis: {title: 'Cantidad'}
-    };
+      y: {
+        label: "Cantidad de multas",
+        domain: [0, 60],
+      },
+      marks: [
+        Plot.line(data1, {x: "hora", y: "cantidad", stroke: "orange"}),
+        Plot.dot(data1, {x: "hora", y: "cantidad", fill: "orange"}),
+        Plot.line(data2, {x: "hora", y: "cantidad", stroke: "maroon"}),
+        Plot.dot(data2, {x: "hora", y: "cantidad", fill: "maroon"}),
+        Plot.text(["17:00"], {
+          x: 17,
+          y: data1.find(d => d.hora === 17).cantidad + 5,
+          fontWeight: "bold",
+          fontSize: 20,
+          fill: "orange",
+        }),
+        Plot.text(["12:00"], {
+          x: 12,
+          y: data2.find(d => d.hora === 12).cantidad + 5,
+          fontWeight: "bold",
+          fontSize: 16,
+          fill: "maroon",
+        })
+      ]
+      
+    });
+      
 
-    var data = [trace1, trace2];
-
-    Plotly.newPlot('chart_3', data, layout);
+    d3.select('#chart_3').append(() => chart);
   });
 });
