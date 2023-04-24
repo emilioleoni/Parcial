9f23b2363e152a16ba8967fc6af6f3d8b64f77da
emilioleoni <emilioleoni12@hotmail.com>
2023-04-24 19:34:22 -0300

    Cambios

--- Scripts/chart_3.js
+++ Scripts/chart_3.js
@@ -29,16 +29,24 @@ d3.csv('CSVS/multas_por_hstotal.csv').then(function(data1) {
         tickValues: d3.range(0, 24),
         tickFormat: x => x,
         line: true,
-        
       },
       y: {
         label: "↑ Cantidad de denuncias",
-        domain: [0, 30],
+        domain: [0, 60],
         line:true,
       },
       marks: [
+        Plot.line(data1, {x: "hora", y: "cantidad", stroke: "orange", strokeWidth: 5}),
+        Plot.dot(data1,{x: 17, y: 50, fill: "orange", r: 6}),
         Plot.line(data2, {x: "hora", y: "cantidad", stroke: "maroon",strokeWidth: 5}),
         Plot.dot(data2, {x: 12, y: 20, fill: "maroon", r: 6}),
+        Plot.text(["17:00"], {
+          x: 17,
+          y: data1.find(d => d.hora === 17).cantidad + 3,
+          fontWeight: "bold",
+          fontSize: 20,
+          fill: "orange",
+        }),
         Plot.text(["12:00"], {
           x: 12,
           y: data2.find(d => d.hora === 12).cantidad + 3,
