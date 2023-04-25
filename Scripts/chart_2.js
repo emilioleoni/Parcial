1679b0599f754c689f03bdd2422c4320f6da248a
ezikaplan <96097788+ezikaplan@users.noreply.github.com>
2023-04-25 12:50:16 -0300

    chart-2 lee csv

--- Scripts/chart_2.js
+++ Scripts/chart_2.js
@@ -1,85 +1,78 @@
-// Reemplazar el contenido de la variable "csvData" con el contenido de tu archivo CSV
-const csvData = `nro;dia;cantidad
-1;Lunes;153
-2;Martes;189
-3;Miercoles;229
-4;Jueves;199
-5;Viernes;202
-6;Sabado;152
-7;Domingo;138`;
-
 // Crear un parser de CSV con el delimitador punto y coma
 const csvParser = d3.dsvFormat(";");
 
-// Parsear los datos del CSV
-const data = csvParser.parse(csvData, d3.autoType);
+// Solicitar el archivo CSV usando la función fetch()
+fetch('CSVS/cant_por_dia.csv')
+  .then(response => response.text())
+  .then(csvData => {
+    // Parsear los datos del CSV
+    const data = csvParser.parse(csvData, d3.autoType);
 
-// Crear una escala de banda personalizada para el eje X
-const xScale = d3.scaleBand()
-  .domain(data.map(d => d.dia))
-  .padding(0.1);
+    // Crear una escala de banda personalizada para el eje X
+    const xScale = d3.scaleBand()
+      .domain(data.map(d => d.dia))
+      .padding(0.1);
 
-// Crear una escala de colores YlOrBr
-const colorScale = d3.scaleSequential()
-  .domain([0, d3.max(data, d => d.cantidad)])
-  .interpolator(d3.interpolateOranges);
+    // Crear una escala de colores YlOrBr
+    const colorScale = d3.scaleSequential()
+      .domain([0, d3.max(data, d => d.cantidad)])
+      .interpolator(d3.interpolateOranges);
 
-// Crear el gráfico de barras
-const chart = Plot.plot({
-    marginBottom: 12,
-    marginTop: 10,
-    style: {
-        fontSize: "22",
-        fontFamily: "Lato, sans-serif",
+    // Crear el gráfico de barras
+    const chart = Plot.plot({
+        marginBottom: 12,
+        marginTop: 10,
+        style: {
+            fontSize: "22",
+            fontFamily: "Lato, sans-serif",
+          },
+      y:{
+        axis:null,
+        fontSize: 10,
+        label: "↑ Cantidad de denuncias",
       },
-  y:{
-    axis:null,
-    fontSize: 10,
-    label: "↑ Cantidad de denuncias",
-  },
-  
-  
-  x: {
-    transform: xScale, // Usar la escala de banda personalizada
-    axis: null, // Eliminar el eje X predeterminado
-  },
-  marks: [
-    Plot.barY(data, {
-      x: "dia",
-      y: "cantidad",
-      fill: (d) => colorScale(d.cantidad), // Usar la escala de colores YlOrBr
-      title: (d) => `Cantidad: ${d.cantidad}`,
-    }),
-    Plot.text(data, {
-      x: "dia",
-      y: (d) => d.cantidad + 5,
-      text: (d) => d.cantidad,
-      textAnchor: "middle",
-      fontFamily: "Lato, sans-serif",
-      fontSize: 20,
-    }),
-    Plot.text (["168"], {
-      x: data.find(d => d.dia === "Jueves"),
-      y: 173,
-      textAnchor: "middle",
-      fontFamily: "Lato, sans-serif",
-      fontWeight: "bold",
-      fontSize: 20,
-    }),
-    // Agregar etiquetas de texto debajo de las barras
-    Plot.text(data, {
-      x: "dia",
-      y: -5,
-      text: "dia",
-      fill: "black",
-      textAnchor: "middle",
-      font: "Lato , sans-serif",
-      fontSize: 20,
-    }),
-  ],
-  
 
-});
 
-// Agregar el gráfico al elemento con id "chart"
-document.getElementById("chart_2").appendChild(chart);
+      x: {
+        transform: xScale, // Usar la escala de banda personalizada
+        axis: null, // Eliminar el eje X predeterminado
+      },
+      marks: [
+        Plot.barY(data, {
+          x: "dia",
+          y: "cantidad",
+          fill: (d) => colorScale(d.cantidad), // Usar la escala de colores YlOrBr
+          title: (d) => `Cantidad: ${d.cantidad}`,
+        }),
+        Plot.text(data, {
+          x: "dia",
+          y: (d) => d.cantidad + 5,
+          text: (d) => d.cantidad,
+          textAnchor: "middle",
+          fontFamily: "Lato, sans-serif",
+          fontSize: 20,
+        }),
+        Plot.text (["168"], {
+          x: data.find(d => d.dia === "Jueves"),
+          y: 173,
+          textAnchor: "middle",
+          fontFamily: "Lato, sans-serif",
+          fontWeight: "bold",
+          fontSize: 20,
+        }),
+        // Agregar etiquetas de texto debajo de las barras
+        Plot.text(data, {
+          x: "dia",
+          y: -5,
+          text: "dia",
+          fill: "black",
+          textAnchor: "middle",
+          font: "Lato , sans-serif",
+          fontSize: 20,
+        }),
+      ],
+    });
+
+    // Agregar el gráfico al elemento con id "chart"
+    document.getElementById("chart_2").appendChild(chart);
+  });
